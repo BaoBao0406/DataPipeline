1ef9a32c459292601cd88c83c9e99fffe3cc166d
BaoBao0406 <46430379+BaoBao0406@users.noreply.github.com>
2021-10-12 17:24:36 +0800

    3rd Update

--- extract_sqlserver_data.py
+++ extract_sqlserver_data.py
@@ -32,30 +32,48 @@ user = user.set_index('Id')['Name'].to_dict()
 
 
 # extract booking info
-BK_tmp = pd.read_sql("SELECT BK.Id, BK.OwnerId, BK.Name, FORMAT(BK.nihrm__ArrivalDate__c, 'MM/dd/yyyy') AS ArrivalDate, FORMAT(BK.nihrm__DepartureDate__c, 'MM/dd/yyyy') AS DepartureDate \
+BK_tmp = pd.read_sql("SELECT BK.Id, BK.OwnerId, BK.Name, FORMAT(BK.nihrm__ArrivalDate__c, 'MM/dd/yyyy') AS ArrivalDate, FORMAT(BK.nihrm__DepartureDate__c, 'MM/dd/yyyy') AS DepartureDate, BK.nihrm__CommissionPercentage__c \
                       FROM dbo.nihrm__Booking__c AS BK \
                       WHERE BK.Booking_ID_Number__c = " + BK_ID_no, conn)
 BK_tmp['OwnerId'].replace(user, inplace=True)
 BK_ID = BK_tmp.iloc[0]['Id']
-
+print(BK_ID)
 
 # extract roomnight_by_day info
-RoomN_tmp = pd.read_sql("SELECT GS.nihrm__Property__c, GS.Name, FORMAT(RoomN.nihrm__PatternDate__c, 'MM/dd/yyyy') AS PatternDate, RoomN.nihrm__AgreedRoomsTotal__c \
+RoomN_tmp = pd.read_sql("SELECT GS.nihrm__Property__c, GS.Name, FORMAT(RoomN.nihrm__PatternDate__c, 'MM/dd/yyyy') AS PatternDate, \
+                             RoomN.nihrm__BlockedRooms1__c, RoomN.nihrm__BlockedRooms2__c, RoomN.nihrm__BlockedRooms3__c, RoomN.nihrm__BlockedRooms4__c, \
+                             RoomN.nihrm__BlockedRate1__c, RoomN.nihrm__BlockedRate2__c, RoomN.nihrm__BlockedRate3__c, RoomN.nihrm__BlockedRate4__c \
                          FROM dbo.nihrm__BookingRoomNight__c AS RoomN \
                          INNER JOIN dbo.nihrm__GuestroomType__c AS GS \
                              ON RoomN.nihrm__GuestroomType__c = GS.Id \
                          WHERE RoomN.nihrm__Booking__c = '" + BK_ID + "'", conn)
+RoomN_tmp.columns = ['Property', 'Room Type', 'Pattern Date', 'Room1', 'Room2', 'Room3', 'Room4', 'Rate1', 'Rate2', 'Rate3', 'Rate4']
+
+# TODO: Find a better solution to fix this
+# Melt Room Night number (4 Occupancy) to columns
+Room_no = RoomN_tmp[['Property', 'Room Type', 'Pattern Date', 'Room1', 'Room2', 'Room3', 'Room4']]
+Room_no = pd.melt(Room_no, id_vars=['Property', 'Room Type', 'Pattern Date'], value_name='Room')
+Room_no['variable'].replace('Room', '', inplace=True, regex=True)
+# Melt Room Rate (4 Occupancy) to columns
+Room_rate = RoomN_tmp[['Property', 'Room Type', 'Pattern Date', 'Rate1', 'Rate2', 'Rate3', 'Rate4']]
+Room_rate = pd.melt(Room_rate, id_vars=['Property', 'Room Type', 'Pattern Date'], value_name='Rate')
+Room_rate['variable'].replace('Rate', '', inplace=True, regex=True)
+convert_to_excel(Room_rate, 'Rate')
+# Join Room Night and Room Rate
+RoomN_tmp = pd.merge(Room_no, Room_rate, on=['Property', 'Room Type', 'Pattern Date', 'variable'])
+
+
 RoomN = 'RoomN'
 convert_to_excel(RoomN_tmp, RoomN)
 
 
 # extract event info
-Event_tmp = pd.read_sql("SELECT ET.Name, FR.Name, ET.nihrm__EventClassificationName__c, ET.nihrm__StartDate__c, nihrm__AgreedEventAttendance__c \
+Event_tmp = pd.read_sql("SELECT ET.Name, FR.Name, ET.nihrm__EventClassificationName__c, FORMAT(ET.nihrm__StartDate__c, 'MM/dd/yyyy') AS Start, ET.nihrm__AgreedEventAttendance__c, ET.nihrm__ForecastAverageCheck1__c, ET.nihrm__ForecastAverageCheck11__c, ET.nihrm__ForecastAverageCheck9__c, ET.nihrm__ForecastAverageCheckFactor9__c, ET.nihrm__ForecastAverageCheck2__c, ET.nihrm__ForecastAverageCheckFactor2__c, ET.nihrm__FunctionRoomRental__c, ET.nihrm__CurrentBlendedRevenue4__c \
                          FROM dbo.nihrm__BookingEvent__c AS ET \
                          INNER JOIN dbo.nihrm__FunctionRoom__c AS FR \
                              ON ET.nihrm__FunctionRoom__c = FR.Id \
                          WHERE ET.nihrm__Booking__c = '" + BK_ID + "'", conn)
-
+Event_tmp.columns = ['Event name', 'Function Space', 'Event Classification', 'Start', '']
 EventT = 'EventT'
 convert_to_excel(Event_tmp, EventT)
 
