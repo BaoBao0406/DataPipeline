98fb6daf4a57489158cbcf57487cc0338bcce89a
BaoBao0406 <46430379+BaoBao0406@users.noreply.github.com>
2021-11-01 17:38:57 +0800

    9th Update
    - update Booking sql

--- extract_sqlserver_data.py
+++ extract_sqlserver_data.py
@@ -13,22 +13,26 @@ def convert_to_excel(data, filename):
     data.to_excel(save_path + filename + '.xlsx', sheet_name='Sheet1', index=False)
 
 
-
+# extract booking info
 def BK_sql_data(BK_ID_no, user, conn):
-    # extract booking info
-    print(BK_ID_no)
-    BK_tmp = pd.read_sql("SELECT BK.Id, BK.OwnerId, BK.Name, FORMAT(BK.nihrm__ArrivalDate__c, 'yyyy/MM/dd') AS ArrivalDate, FORMAT(BK.nihrm__DepartureDate__c, 'yyyy/MM/dd') AS DepartureDate, BK.nihrm__CommissionPercentage__c, BK.nihrm__Property__c, BK.nihrm__FoodBeverageMinimum__c \
+    # Booking SQL
+    BK_tmp = pd.read_sql("SELECT BK.Id, BK.OwnerId, BK.Name, FORMAT(BK.nihrm__ArrivalDate__c, 'yyyy-MM-dd') AS ArrivalDate, FORMAT(BK.nihrm__DepartureDate__c, 'yyyy-MM-dd') AS DepartureDate, BK.nihrm__CommissionPercentage__c, BK.Percentage_of_Attrition__c, BK.nihrm__Property__c, BK.nihrm__FoodBeverageMinimum__c, ac.Name AS ACName, ag.Name AS AGName, BK.End_User_Region__c, BK.End_User_SIC__c, BK.nihrm__BookingTypeName__c \
                           FROM dbo.nihrm__Booking__c AS BK \
+                              LEFT JOIN dbo.Account AS ac \
+                                  ON BK.nihrm__Account__c = ac.Id \
+                              LEFT JOIN dbo.Account AS ag \
+                                  ON BK.nihrm__Agency__c = ag.Id \
                           WHERE BK.Booking_ID_Number__c = " + BK_ID_no, conn)
     BK_tmp['OwnerId'].replace(user, inplace=True)
     # pull the actual booking ID in FDC
     BK_ID = BK_tmp.iloc[0]['Id']
     
     return BK_tmp, BK_ID
-    
-    
+
+
+# extract roomnight_by_day info function
 def RoomN_sql_data(BK_ID, conn):
-    # extract roomnight_by_day info
+    # Room SQL
     RoomN_tmp = pd.read_sql("SELECT GS.nihrm__Property__c, GS.Name, FORMAT(RoomN.nihrm__PatternDate__c, 'yyyy/MM/dd') AS PatternDate, \
                                  RoomN.nihrm__BlockedRooms1__c, RoomN.nihrm__BlockedRooms2__c, RoomN.nihrm__BlockedRooms3__c, RoomN.nihrm__BlockedRooms4__c, \
                                  RoomN.nihrm__BlockedRate1__c, RoomN.nihrm__BlockedRate2__c, RoomN.nihrm__BlockedRate3__c, RoomN.nihrm__BlockedRate4__c \
@@ -54,8 +58,9 @@ def RoomN_sql_data(BK_ID, conn):
     return RoomN_tmp
 
 
+# extract event info function
 def Event_sql_data(BK_ID, conn):
-    # extract event info
+    # Event SQL
     Event_tmp = pd.read_sql("SELECT ET.Name, FR.Name, ET.nihrm__EventClassificationName__c, FORMAT(ET.nihrm__StartDate__c, 'yyyy/MM/dd') AS Start, ET.nihrm__AgreedEventAttendance__c, ET.nihrm__ForecastAverageCheck1__c, ET.nihrm__ForecastAverageCheck1__c, ET.nihrm__ForecastRevenue1__c, ET.nihrm__ForecastAverageCheck9__c, ET.nihrm__ForecastAverageCheckFactor9__c, ET.nihrm__ForecastRevenue9__c, ET.nihrm__ForecastAverageCheck2__c, ET.nihrm__ForecastAverageCheckFactor2__c, ET.nihrm__ForecastRevenue2__c, ET.nihrm__FunctionRoomRental__c, ET.nihrm__CurrentBlendedRevenue4__c \
                              FROM dbo.nihrm__BookingEvent__c AS ET \
                              INNER JOIN dbo.nihrm__FunctionRoom__c AS FR \
@@ -68,7 +73,7 @@ def Event_sql_data(BK_ID, conn):
 
 
 # main function for extract_sqlserver_data
-def sqlserver_data(table):
+def extract_sqlserver_data(table):
     
     # for testing purpose
 #    table = pd.read_csv(os.path.abspath(os.getcwd()) + '\\tmp.csv')
@@ -89,11 +94,11 @@ def sqlserver_data(table):
     user = user.set_index('Id')['Name'].to_dict()
     
     
-    
+    # load Booking info function
     BK_tmp, BK_ID = BK_sql_data(BK_ID_no, user, conn)
-    
+    # load Room info function
     RoomN_tmp = RoomN_sql_data(BK_ID, conn)
-    
+    # load Event info function
     Event_tmp = Event_sql_data(BK_ID, conn)
 
     return BK_tmp, RoomN_tmp, Event_tmp
@@ -101,7 +106,7 @@ def sqlserver_data(table):
 
 
 # Data Checking
-#BK_tmp, RoomN_tmp, Event_tmp = sqlserver_data()
+#BK_tmp, RoomN_tmp, Event_tmp = extract_sqlserver_data()
 #
 #BK = 'BKinfo'
 #convert_to_excel(BK_tmp, BK)
