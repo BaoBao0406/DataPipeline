b7ef3345829d1d6db3ec623b7971b74cf5b5555c
BaoBao0406 <46430379+BaoBao0406@users.noreply.github.com>
2021-11-08 17:34:23 +0800

    3rd Update

--- main.py
+++ main.py
@@ -3,35 +3,29 @@
 
 import pandas as pd
 import datetime, os.path
-#import outlook_trigger, extract_sqlserver_data, proforma_sync
-import extract_sqlserver_data
+import outlook_trigger, extract_sqlserver_data, proforma_sync
 
-
-# Convert data to excel format
-def convert_to_excel(data, filename):
-    data.to_excel(save_path + filename + '.xlsx', sheet_name='Sheet1', index=False)
     
-save_path = 'I:\\10-Sales\\Personal Folder\\Admin & Assistant Team\\Patrick Leong\\Python Code\\DataPipeline\\'
+#tmp_path = 'I:\\10-Sales\\+Dept Admin (3Y, Internal)\\2021\\Personal Folders\\Patrick Leong\\Python Code\\DataPipeline\\'
 
 # Run outlook_trigger function
-#outlook_trigger.outlook_trigger()
+outlook_trigger.outlook_trigger()
 
 # Run extract_sqlserver_data function if there is any data save in csv
 if os.path.exists(os.getcwd() + '\\tmp.csv'):
-# TODO: fix the bugs
-#    table = pd.read_csv(os.path.abspath(os.getcwd()) + 'tmp.csv')
-    table = pd.read_csv(save_path + 'tmp.csv')
+
+    table = pd.read_csv(os.path.abspath(os.getcwd()) + '\\tmp.csv')
     number_of_booking = table.shape[0]
     
     # Run every booking in each row and pass Booking ID to extract_sqlserver_data Function
     for i in range(number_of_booking):
         col = table.iloc[i, :]
-        BK_tmp, RoomN_tmp, Event_tmp = extract_sqlserver_data.sqlserver_data(col)
-        
+        BK_tmp, RoomN_tmp, Event_tmp = extract_sqlserver_data.extract_sqlserver_data(col)
+        proforma_sync.proforma_sync(BK_tmp, RoomN_tmp, Event_tmp)
         
         
 #        BK = 'BKinfo' + str(i)
-#        convert_to_excel(BK_tmp, BK)
+#        convert_to_excel(BK_tmp, BK)                                                                                                                                                   
 #        
 #        Room = 'RoomN'+ str(i)
 #        convert_to_excel(RoomN_tmp, Room)
