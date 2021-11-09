736b1feb6bd572d92c9d9c9bc105646d4c4643a7
BaoBao0406 <46430379+BaoBao0406@users.noreply.github.com>
2021-11-09 10:34:56 +0800

    4th Update
    - bug fix
    - using os path to save file
    - add br function

--- main.py
+++ main.py
@@ -3,7 +3,7 @@
 
 import pandas as pd
 import datetime, os.path
-import outlook_trigger, extract_sqlserver_data, proforma_sync
+import outlook_trigger, extract_sqlserver_data, proforma_sync, business_review_sync
 
     
 #tmp_path = 'I:\\10-Sales\\+Dept Admin (3Y, Internal)\\2021\\Personal Folders\\Patrick Leong\\Python Code\\DataPipeline\\'
@@ -21,8 +21,10 @@ if os.path.exists(os.getcwd() + '\\tmp.csv'):
     for i in range(number_of_booking):
         col = table.iloc[i, :]
         BK_tmp, RoomN_tmp, Event_tmp = extract_sqlserver_data.extract_sqlserver_data(col)
+        # Run proforma main function
         proforma_sync.proforma_sync(BK_tmp, RoomN_tmp, Event_tmp)
-        
+        # Run BR main function
+        business_review_sync.business_review_sync(BK_tmp, RoomN_tmp, Event_tmp)
         
 #        BK = 'BKinfo' + str(i)
 #        convert_to_excel(BK_tmp, BK)                                                                                                                                                   
@@ -33,15 +35,7 @@ if os.path.exists(os.getcwd() + '\\tmp.csv'):
 #        Event = 'EventT' + str(i)
 #        convert_to_excel(Event_tmp, Event)
 
-        # TODO: transform data to Proforma 
-        
-# TODO: transform data to BR
         
 # TODO: Convert text for BookingComment
 
 # TODO: Using model to
-
-
-
-
-
