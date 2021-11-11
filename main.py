5b3f7a579577186d59aa88aa8ccd9853c62cc4fe
BaoBao0406 <46430379+BaoBao0406@users.noreply.github.com>
2021-11-11 14:25:23 +0800

    6th Update
    - add select to run BR or BP function, and breakfast inclusive selection

--- main.py
+++ main.py
@@ -2,7 +2,7 @@
 # main.py - 
 
 import pandas as pd
-import datetime, os.path
+import datetime, os.path, os
 import outlook_trigger, extract_sqlserver_data, proforma_sync, business_review_sync
     
 #tmp_path = 'I:\\10-Sales\\+Dept Admin (3Y, Internal)\\2021\\Personal Folders\\Patrick Leong\\Python Code\\DataPipeline\\'
@@ -18,22 +18,27 @@ if os.path.exists(os.getcwd() + '\\tmp.csv'):
     
     # Run every booking in each row and pass Booking ID to extract_sqlserver_data Function
     for i in range(number_of_booking):
-        col = table.iloc[i, :]
-        BK_tmp, RoomN_tmp, Event_tmp = extract_sqlserver_data.extract_sqlserver_data(col)
-        # Run proforma main function
-        proforma_sync.proforma_sync(BK_tmp, RoomN_tmp, Event_tmp)
-        # Run BR main function
-        business_review_sync.business_review_sync(BK_tmp, RoomN_tmp, Event_tmp)
+        # get row of Booking info and pass to sql to run data
+        bk_row = table.iloc[i, :]
+        BK_tmp, RoomN_tmp, Event_tmp = extract_sqlserver_data.extract_sqlserver_data(bk_row)
         
-#        BK = 'BKinfo' + str(i)
-#        convert_to_excel(BK_tmp, BK)                                                                                                                                                   
-#        
-#        Room = 'RoomN'+ str(i)
-#        convert_to_excel(RoomN_tmp, Room)
-#        
-#        Event = 'EventT' + str(i)
-#        convert_to_excel(Event_tmp, Event)
-
+        # proforma main function
+        run_BP = str(bk_row['Proforma']).lower()
+        if run_BP == 'yes':
+            # Run proforma main function
+            proforma_sync.proforma_sync(BK_tmp, RoomN_tmp, Event_tmp)
+            
+        # BR main function
+        run_BR = str(bk_row['Business Review']).lower()
+        if run_BR == 'yes':
+            # Check if this is bbf inclusive
+            bbf_inc = str(bk_row['Breakfast inclusive']).lower()
+            # Run BR main function
+            business_review_sync.business_review_sync(BK_tmp, RoomN_tmp, Event_tmp, bbf_inc)
+    
+    # remove tmp file
+    os.remove(os.getcwd() + '\\tmp.csv')
+    
         
 # TODO: Convert text for BookingComment
 
