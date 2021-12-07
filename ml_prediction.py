67f239017b04134e8beb15edb2e9764aaa7e9f55
BaoBao0406 <46430379+BaoBao0406@users.noreply.github.com>
2021-12-07 16:52:17 +0800

    2nd Update

--- ml_prediction.py
+++ ml_prediction.py
@@ -1,7 +1,37 @@
 import pyodbc
 import pandas as pd
 import datetime, os.path
+import numpy as np
 
+#################################################
+
+save_path = 'I:\\10-Sales\\+Dept Admin (3Y, Internal)\\2021\\Personal Folders\\Patrick Leong\\Python Code\\DataPipeline\\Testing files\\'
+
+#table = pd.read_csv(os.path.abspath(os.getcwd()) + '\\tmp.csv')
+
+
+# Convert data to excel format
+#def convert_to_excel(data, filename):
+#    data.to_excel(save_path + filename + '.xlsx', sheet_name='Sheet1')
+
+#col = table.iloc[0]['Booking ID']
+# Testing booking with BK_ID directly
+BK_ID_no = '014760'
+#BK_ID_no = str(int(col)).zfill(6)
+
+
+conn = pyodbc.connect('Driver={SQL Server};'
+                      'Server=VOPPSCLDBN01\VOPPSCLDBI01;'
+                      'Database=SalesForce;'
+                      'Trusted_Connection=yes;')
+
+
+# FDC User ID and Name list
+user = pd.read_sql('SELECT DISTINCT(Id), Name \
+                    FROM dbo.[User]', conn)
+user = user.set_index('Id')['Name'].to_dict()
+
+#################################################
 
 # extract booking info
 def BK_sql_data(BK_ID_no, user, conn):
@@ -70,48 +100,93 @@ def Event_sql_data(BK_ID, conn):
 # TODO: RoomB sql for rates
 def RoomB_sql_data(BK_ID, conn):
     RoomB_tmp = pd.read_sql("SELECT RoomB.nihrm__Location__c, ac.nihrm__RegionName__c, ac.Industry, ag.Name, ag.Industry, BK.End_User_Region__c, BK.End_User_SIC__c, BK.nihrm__BookingTypeName__c, BK.nihrm__CurrentBlendedRoomnightsTotal__c, BK.VCL_Blended_F_B_Revenue__c, BK.nihrm__CurrentBlendedEventRevenue7__c, BK.nihrm__CurrentBlendedEventRevenue4__c, BK.RSO_Manager__c, BK.nihrm__BookingMarketSegmentName__c, BK.Promotion__c, RoomB.nihrm__PeakRoomnightsAgreed__c, \
-                                 RoomB.nihrm__CurrentBlendedADR__c,  \
+                                    RoomB.nihrm__CurrentBlendedADR__c, FORMAT(RoomB.nihrm__StartDate__c, 'yyyy/MM/dd') AS StartDate, FORMAT(RoomB.nihrm__EndDate__c, 'yyyy/MM/dd') AS EndDate \
                              FROM dbo.nihrm__Booking__c AS BK \
-                                 INNER JOIN dbo.nihrm__RoomBlock__c AS RoomB \
-                                     ON BK.nihrm__Booking__c = RoomB.nihrm__Booking__c \
+                                 INNER JOIN dbo.nihrm__BookingRoomBlock__c AS RoomB \
+                                     ON BK.Id = RoomB.nihrm__Booking__c \
                                  LEFT JOIN dbo.Account AS ac \
                                      ON BK.nihrm__Account__c = ac.Id \
                                  LEFT JOIN dbo.Account AS ag \
                                      ON BK.nihrm__Agency__c = ag.Id \
-                             WHERE BK.nihrm__Booking__c = " + BK_ID, conn)
+                             WHERE BK.Id = '" + BK_ID + "'", conn)
     
     return RoomB_tmp
 
-# TODO: Use Event_tmp for max Attendance for booking and room block BK & RoomBlock
-attendance = Event_tmp['Agreed'].max()
+BK_tmp, BK_ID = BK_sql_data(BK_ID_no, user, conn)
+RoomN_tmp = RoomN_sql_data(BK_ID, conn)
+Event_tmp = Event_sql_data(BK_ID, conn)
+RoomB_tmp = RoomB_sql_data(BK_ID, conn)
 
+#################################################
+
+# TODO: Use Event_tmp for max Attendance for booking and room block BK & RoomBlock
+if Event_tmp['Agreed'].max() is not np.NaN:
+    attendance = int(Event_tmp['Agreed'].max())
+else:
+    attendance = 0
+
+# rename BK_tmp column
+BK_tmp.columns = ['Id', 'BK_no', 'Owner', 'Post As', 'ArrivalDate', 'DepartureDate', 'Commission', 'Attrition', 'Property', 'F&B Minimum', 'Account', 'Agency', 'End User Region',
+                  'End User SIC', 'Booking Type', 'RSO Manager', 'Non-compete', 'Account: Region', 'Account: Industry', 'Blended Roomnights', 'Blended Guestroom Revenue Total',
+                  'Blended F&B Revenue', 'Blended Rental Revenue', 'Blended AV Revenue', 'Market Segment', 'Promotion', 'Blended ADR', 'Peak Roomnights Blocked', 
+                  'BookedDate', 'LastStatusDate']
+BK_tmp['Attendance'] = int(attendance)
 # TODO: booking info for materization percentage
 BK_mat_percent = BK_tmp[['Property', 'Account: Region', 'Account: Industry', 'Agency', 'End User Region', 'End User SIC', 'Booking Type', 'Blended Roomnights',
                         'Blended Guestroom Revenue Total', 'Blended F&B Revenue', 'Blended Rental Revenue', 'Blended AV Revenue', 'Attendance',
                         'RSO Manager', 'Market Segment', 'Promotion', 'Blended ADR', 'Peak Roomnights Blocked', 'ArrivalDate', 'DepartureDate', 
                         'BookedDate', 'LastStatusDate']]
 # Maximum attendance
-BK_mat_percent['Attendance'] = int(attendance)
+
 # calculate Inhouse day (Departure - Arrival)    
 BK_mat_percent['Inhouse day'] = pd.to_datetime(BK_mat_percent['DepartureDate']).dt.date - pd.to_datetime(BK_mat_percent['ArrivalDate']).dt.date
 # calculate Lead day (Arrival - Booked) 
 BK_mat_percent['Lead day'] = pd.to_datetime(BK_mat_percent['ArrivalDate']).dt.date - pd.to_datetime(BK_mat_percent['BookedDate']).dt.date
 # calculate Decision day (Last Status date - Booked) 
 BK_mat_percent['Decision day'] = pd.to_datetime(BK_mat_percent['LastStatusDate']).dt.date - pd.to_datetime(BK_mat_percent['BookedDate']).dt.date
+# booking info Arrival Month
+BK_mat_percent['Arrival Month'] = pd.DatetimeIndex(BK_mat_percent['ArrivalDate']).month
+# booking info Booked Month
+BK_mat_percent['Booked Month'] = pd.DatetimeIndex(BK_mat_percent['BookedDate']).month
+# booking info Last Status Month
+BK_mat_percent['Last Status Month'] = pd.DatetimeIndex(BK_mat_percent['LastStatusDate']).month
+# 
+BK_mat_percent = BK_mat_percent[['Property', 'Account: Region', 'Account: Industry', 'Agency', 'End User Region', 'End User SIC', 'Booking Type', 'Blended Roomnights', 'Blended Guestroom Revenue Total', 
+                                 'Blended F&B Revenue', 'Blended Rental Revenue', 'Blended AV Revenue', 'Attendance', 'RSO Manager', 'Market Segment', 'Promotion', 'Blended ADR', 'Peak Roomnights Blocked', 
+                                 'Inhouse day', 'Lead day', 'Decision day', 'Arrival Month', 'Booked Month', 'Last Status Month']]
+
+print(BK_mat_percent.T)
 
 
+# rename RoomB_tmp column
+RoomB_tmp.columns = ['Property', 'Account: Region', 'Account: Industry', 'Agency: Account Name', 'Agency: Industry', 'Booking: End User Region', 'Booking: End User SIC', 
+                     'Booking: Booking Type', 'Blended Roomnights', 'Booking: Blended F&B Revenue', 'Booking: Blended Rental Revenue', 'Booking: Blended AV Revenue',
+                     'Booking: RSO Manager', 'Booking: Market Segment', 'Booking: Promotion', 'Peak Roomnights Agreed', 'Blended ADR', 'Start', 'End']
 # TODO: room block info for ADR rate
-RB_adr_rate = RoomB_tmp[['']]
+RB_adr_rate = RoomB_tmp
 # Maximum attendance
-RB_adr_rate['Attendance'] = int(attendance)
-# calculate Inhouse day (Departure - Arrival) - via RoomN_tmp
-RB_adr_rate['Inhouse day'] = RoomN_tmp['Pattern Date'].max() - RoomN_tmp['Pattern Date'].min()
-# calculate Lead day (Arrival - Booked) - via RoomN_tmp
-RB_adr_rate['Lead day'] = RoomN_tmp['Pattern Date'].min() - pd.to_datetime(BK_mat_percent['BookedDate']).dt.date
-# TODO: calculate Decision day (Last Status date - Booked) - via RoomN_tmp
-RB_adr_rate['Decision day'] = pd.to_datetime(BK_mat_percent['LastStatusDate']).dt.date - pd.to_datetime(BK_mat_percent['BookedDate']).dt.date
+RB_adr_rate['Attendance'] = attendance
+# calculate Inhouse day (Departure - Arrival)
+RB_adr_rate['Inhouse day'] = pd.to_datetime(RoomB_tmp['End']).dt.date - pd.to_datetime(RoomB_tmp['Start']).dt.date
+# calculate Lead day (Arrival - Booked)
+RB_adr_rate['Lead day'] = BK_tmp['BookedDate'].values[0]
+RB_adr_rate['Lead day'] = pd.to_datetime(RoomB_tmp['Start']).dt.date - pd.to_datetime(RoomB_tmp['Lead day']).dt.date
+# calculate Decision day (Last Status date - Booked)
+decision_day_rb = (pd.to_datetime(BK_tmp['LastStatusDate']).dt.date - pd.to_datetime(BK_tmp['BookedDate']).dt.date).values[0]
+RB_adr_rate['Decision day'] = decision_day_rb
+# room block info Arrival Month
+RB_adr_rate['Arrival Month'] = pd.DatetimeIndex(RoomB_tmp['Start']).month
+# room block info Booked Month
+booked_month_rb = (pd.DatetimeIndex(BK_tmp['BookedDate']).month).values[0]
+RB_adr_rate['Booked Month'] = booked_month_rb
+# room block info Last Status Month
+last_status_rb = (pd.DatetimeIndex(BK_tmp['LastStatusDate']).month).values[0]
+RB_adr_rate['Last Status Month'] = last_status_rb
+# 
+RB_adr_rate = RB_adr_rate[['Property', 'Account: Region', 'Account: Industry', 'Agency: Account Name', 'Booking: End User Region', 'Booking: End User SIC', 'Booking: Booking Type', 'Blended Roomnights',
+                           'Booking: Blended F&B Revenue', 'Booking: Blended Rental Revenue', 'Booking: Blended AV Revenue', 'Attendance', 'Booking: RSO Manager', 'Booking: Market Segment', 'Booking: Promotion', 
+                           'Peak Roomnights Agreed', 'Inhouse day', 'Lead day', 'Decision day', 'Arrival Month', 'Booked Month', 'Last Status Month']]
+
+print(RB_adr_rate)
+
 # TODO: take out Blended Revenue Total in RoomBlock
-
-
-['Arrival Month', 'Booked Month', 'Last Status Month']
-
