db48d86668290978217353c6099b1d98503730ae
BaoBao0406 <46430379+BaoBao0406@users.noreply.github.com>
2021-12-15 15:26:04 +0800

    11th Update
    - amend for oversize event table

--- main.py
+++ main.py
@@ -24,31 +24,34 @@ if os.path.exists(os.getcwd() + '\\tmp.csv'):
         
         # proforma main function
         run_BP = str(bk_row['Proforma']).lower()
+        # Set default value for BP_file_path
+        bk_row['BP_file_path'] = 'Not Available'
         if run_BP == 'yes':
             # Run proforma main function
             BP_file_path = proforma_sync.proforma_sync(BK_tmp, RoomN_tmp, Event_tmp)
             # Save path to table
             bk_row['BP_file_path'] = BP_file_path
-        else:
-            bk_row['BP_file_path'] = 'Not Available'
-            
+        
         # BR main function
         run_BR = str(bk_row['Business Review']).lower()
+        # Set default value for BR_file_path
+        bk_row['BR_file_path'] = 'Not Available'
+        # Boolean for event size if above 260 rows
+        oversize_event_table = False
         if run_BR == 'yes':
             # Check if this is bbf inclusive
             bbf_inc = str(bk_row['Breakfast inclusive']).lower()
             # Run BR main function
-            BR_file_path = business_review_sync.business_review_sync(BK_tmp, RoomN_tmp, Event_tmp, bbf_inc)
+            BR_file_path, oversize_event_table = business_review_sync.business_review_sync(BK_tmp, RoomN_tmp, Event_tmp, bbf_inc, oversize_event_table)
             # Save path to table
             bk_row['BR_file_path'] = BR_file_path
-        else:
-            bk_row['BR_file_path'] = 'Not Available'
     
         # send email to reply with BR and BP path link
-        outlook_trigger.reply_notification(bk_row)
-    
+        outlook_trigger.reply_notification(bk_row, oversize_event_table)
+
     # remove tmp file
     os.remove(os.getcwd() + '\\tmp.csv')
+
       
 # TODO: Convert text for BookingComment
 
