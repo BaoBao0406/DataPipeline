d220abfe15b0c05017b5763b19b33b7fec04634f
BaoBao0406 <46430379+BaoBao0406@users.noreply.github.com>
2021-12-16 17:30:12 +0800

    12th Update
    - add try except error to catch bugs
    - delete msg if cannot run BR and BP function successfully
    - add logging to track error

--- main.py
+++ main.py
@@ -2,9 +2,12 @@
 # main.py - 
 
 import pandas as pd
-import datetime, os.path, os
+import datetime, os.path, os, logging
 import outlook_trigger, extract_sqlserver_data, proforma_sync, business_review_sync
-    
+
+# logging error into file
+log_file = os.path.abspath(os.getcwd()) + '\\error.log'
+logging.basicConfig(level=logging.ERROR, filename=log_file, filename='w')    
 
 # Run outlook_trigger function
 outlook_trigger.outlook_trigger()
@@ -20,35 +23,47 @@ if os.path.exists(os.getcwd() + '\\tmp.csv'):
     for i in range(number_of_booking):
         # get row of Booking info and pass to sql to run data
         bk_row = table.iloc[i, :]
-        BK_tmp, RoomN_tmp, Event_tmp = extract_sqlserver_data.extract_sqlserver_data(bk_row)
         
-        # proforma main function
-        run_BP = str(bk_row['Proforma']).lower()
-        # Set default value for BP_file_path
-        bk_row['BP_file_path'] = 'Not Available'
-        if run_BP == 'yes':
-            # Run proforma main function
-            BP_file_path = proforma_sync.proforma_sync(BK_tmp, RoomN_tmp, Event_tmp)
-            # Save path to table
-            bk_row['BP_file_path'] = BP_file_path
+        try:
+            # extract sql from sqlserver main function
+            BK_tmp, RoomN_tmp, Event_tmp = extract_sqlserver_data.extract_sqlserver_data(bk_row)
         
-        # BR main function
-        run_BR = str(bk_row['Business Review']).lower()
-        # Set default value for BR_file_path
-        bk_row['BR_file_path'] = 'Not Available'
-        # Boolean for event size if above 260 rows
-        oversize_event_table = False
-        if run_BR == 'yes':
-            # Check if this is bbf inclusive
-            bbf_inc = str(bk_row['Breakfast inclusive']).lower()
-            # Run BR main function
-            BR_file_path, oversize_event_table = business_review_sync.business_review_sync(BK_tmp, RoomN_tmp, Event_tmp, bbf_inc, oversize_event_table)
-            # Save path to table
-            bk_row['BR_file_path'] = BR_file_path
-    
-        # send email to reply with BR and BP path link
-        outlook_trigger.reply_notification(bk_row, oversize_event_table)
-
+            # proforma main function
+            run_BP = str(bk_row['Proforma']).lower()
+            # Set default value for BP_file_path
+            bk_row['BP_file_path'] = 'Not Available'
+            if run_BP == 'yes':
+                # Run proforma main function
+                BP_file_path = proforma_sync.proforma_sync(BK_tmp, RoomN_tmp, Event_tmp)
+                # Save path to table
+                bk_row['BP_file_path'] = BP_file_path
+            
+            # BR main function
+            run_BR = str(bk_row['Business Review']).lower()
+            # Set default value for BR_file_path
+            bk_row['BR_file_path'] = 'Not Available'
+            # Boolean for event size if above 260 rows
+            oversize_event_table = False
+            if run_BR == 'yes':
+                # Check if this is bbf inclusive
+                bbf_inc = str(bk_row['Breakfast inclusive']).lower()
+                # Run BR main function
+                BR_file_path, oversize_event_table = business_review_sync.business_review_sync(BK_tmp, RoomN_tmp, Event_tmp, bbf_inc, oversize_event_table)
+                # Save path to table
+                bk_row['BR_file_path'] = BR_file_path
+        
+            # send email to reply with BR and BP path link
+            outlook_trigger.reply_notification(bk_row, oversize_event_table)
+        
+        except Exception as e:
+            # Remove msg file if error occurs
+            os.remove(str(bk_row['msg_path']))
+            # log the error to log file
+            logging.exception('Booking ID: ' + str(bk_row['Booking ID']))
+            # TODO: function to send notification for failing to run
+            
+            pass
+            
     # remove tmp file
     os.remove(os.getcwd() + '\\tmp.csv')
 
